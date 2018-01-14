2ec33eb6428fd0f458eac0e7423342b69fd0e4cd
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2018-01-14 19:08:38 +0100

    release 0.1.1

--- csv23/__init__.py
+++ csv23/__init__.py
@@ -17,7 +17,7 @@ __all__ = [
 ]
 
 __title__ = 'csv23'
-__version__ = '0.1.1.dev0'
+__version__ = '0.1.1'
 __author__ = 'Sebastian Bank <sebastian.bank@uni-leipzig.de>'
 __license__ = 'MIT, see LICENSE.txt'
 __copyright__ = 'Copyright (c) 2018 Sebastian Bank'
