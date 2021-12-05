94266ce8a7fb9254549f41e99da61e9a77bd760e
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2021-12-05 16:09:59 +0100

    increase verbosity

--- run-tests.py
+++ run-tests.py
@@ -1,19 +1,32 @@
 #!/usr/bin/env python3
 
+"""Run the tests with https://pytest.org."""
+
+from __future__ import print_function
+
+import os
 import platform
 import sys
 
 import pytest
 
-ARGS = [
-    #'--pdb',
-    #'--exitfirst',
-]
+ARGS = [#'--skip-exe',
+        #'--only-exe',
+        #'--collect-only',
+        #'--verbose',
+        #'--pdb',
+        #'--exitfirst',  # a.k.a. -x
+        #'-W', 'error',
+       ]
 
 if platform.system() == 'Windows':
     if 'idlelib' in sys.modules:
-        ARGS.extend(['--capture=sys', '--color=no'])
+        ARGS += ['--capture=sys', '--color=no']
     elif sys.version_info.major == 2 and 'win_unicode_console' in sys.modules:
         ARGS.append('--capture=sys')
 
-sys.exit(pytest.main(ARGS + sys.argv[1:]))
+print('run', [os.path.basename(__file__)] + sys.argv[1:])
+args = ARGS + sys.argv[1:]
+
+print('pytest.main({!r})'.format(args))
+sys.exit(pytest.main(args))
