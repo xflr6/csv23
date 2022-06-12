9159ca5f06f8e1d7ce619f5c03a621c48c09c265
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2022-06-12 14:23:03 +0200

    release 0.3.4

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2018-2021, Sebastian Bank'
 author = 'Sebastian Bank'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.4.dev0'
+release = '0.3.4'
 
 
 # -- General configuration ---------------------------------------------------
